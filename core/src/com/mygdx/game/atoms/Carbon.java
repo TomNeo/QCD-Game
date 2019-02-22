7dab9c47f3fc7cbd27a5749f3f50e9169e6e7756
ministeve412 <ministeve412@gmail.com>
2019-02-21 16:19:02 -0800

    merging with master, do we want all ifs running or only one?

--- core/src/com/mygdx/game/atoms/Carbon.java
+++ core/src/com/mygdx/game/atoms/Carbon.java
@@ -59,28 +59,35 @@ public class Carbon extends Circles {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.line(pos.x, pos.y,this.timerX,this.timerY);
         shapeRenderer.end();
-        if(protons >= 1){
+        if(protons == 1){
             shapeRenderer.setColor(1, 1, 1, .4f);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             shapeRenderer.circle(pos.x, pos.y + radius/2, Variables.PROTON_RADIUS);
-        }
-        if(protons >= 2){
+            shapeRenderer.end();
+        }else if (protons == 2){
             shapeRenderer.setColor(1, 1, 1, .4f);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             shapeRenderer.circle(pos.x - radius/ 4 * (float)Math.sqrt(2), pos.y - radius / 4 *(float)Math.sqrt(2), Variables.PROTON_RADIUS);
-        }
-        if(protons >= 3){
+            shapeRenderer.end();
+        }else if (protons >= 3){
             shapeRenderer.setColor(1, 1, 1, .4f);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             shapeRenderer.circle(pos.x + radius/ 4 * (float)Math.sqrt(2), pos.y - radius / 4 *(float)Math.sqrt(2), Variables.PROTON_RADIUS);
+            shapeRenderer.end();
         }
+
         if(game.highlightedCircle != null && (game.highlightedCircle.getClass() == Proton.class) && !this.equals(game.highlightedCircle)){
             shapeRenderer.setColor(Variables.MATCH_INDICATOR_COLOR[0], Variables.MATCH_INDICATOR_COLOR[1],Variables.MATCH_INDICATOR_COLOR[2],Variables.MATCH_INDICATOR_COLOR[3]);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             shapeRenderer.circle(pos.x, pos.y, Variables.MATCH_INDICATOR_RADIUS);
+            shapeRenderer.end();
         }
     }
 
     public void collided(){
     }
 
-    public void captureProton(){
+    void captureProton(){
         protons++;
         if(lifeSpan + Variables.PROTON_CAPTURE_LIFESPAN_BUMP > Variables.CARBON_LIFESPAN){
             lifeSpan = Variables.CARBON_LIFESPAN;
