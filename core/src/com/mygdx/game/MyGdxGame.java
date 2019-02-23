85047a7b5153055e00941f1c214ccafe978cfe22
ministeve412 <ministeve412@gmail.com>
2019-02-22 17:37:08 -0800

    adding skin object for labels and other UI

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.game.atoms.Circles;
 import com.mygdx.game.screens.MenuScreen;
 
@@ -23,6 +24,7 @@ public class MyGdxGame extends Game {
 	public SpriteBatch batch;
     public ShapeRenderer shapeRenderer;
     public Circles highlightedCircle = null;
+    public Skin skin;
     public Sound soundEffect;
 
 	@Override
@@ -31,11 +33,14 @@ public class MyGdxGame extends Game {
 
         shapeRenderer = new ShapeRenderer();
 
+		skin = new Skin();
+
         generator = new FreeTypeFontGenerator(Gdx.files.internal("SecretST.TTF"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.color = Color.YELLOW;
         parameter.size = 40;
         font = generator.generateFont(parameter);
+        skin.add("font", font, BitmapFont.class);
 
         if (Gdx.app.getType() == Application.ApplicationType.Android ||
 				Gdx.app.getType() == Application.ApplicationType.Desktop) {
@@ -54,6 +59,13 @@ public class MyGdxGame extends Game {
 
 	@Override
 	public void dispose () {
+
+		if (skin != null) {
+            if (skin.has("font", BitmapFont.class))
+                skin.remove("font", BitmapFont.class);
+			skin.dispose();
+		}
+
 		batch.dispose();
 		font.dispose();
 		generator.dispose();
