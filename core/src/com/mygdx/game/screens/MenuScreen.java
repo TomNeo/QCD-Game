c4785d7bc37a854401882bd303e40fcaa242f52c
TomNeo <TomNeo14@gmail.com>
2019-03-28 07:55:06 -0400

    Fun visual stuff on the main menu

--- core/src/com/mygdx/game/screens/MenuScreen.java
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -17,16 +17,29 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Variables;
 
 public class MenuScreen implements Screen {
 
     private MyGdxGame game;
     private Stage stageUI;
     private ParticleEffect effect;
+    float currentDegrees = 0;
+    int radiusFlux = 0;
+    int radiusChange = 0;
+
+    float P0x = 40;
+    float P0y = 140;
+    float P1x = 80;
+    float P1y = 140 + 70.71f;
+    float P2x = 120;
+    float P2y = 140;
 
     //create paths
     private Bezier<Vector2> path1;
+    private Bezier<Vector2> path3;
     private CatmullRomSpline<Vector2> path2;
+    private Bezier<Vector2>[] circlePath;
 
     public MenuScreen(MyGdxGame gam) {
         game = gam;
@@ -84,9 +97,16 @@ public class MenuScreen implements Screen {
             controlPoints[i] = point;
         }
 
+        Vector2[] RossPoints = new Vector2[]{new Vector2(P0x, P0y), new Vector2(P1x, P1y), new Vector2(P2x, P2y)};
+
+
+
+
+
         // set up the curves
         path1 = new Bezier<Vector2>(controlPoints);
         path2 = new CatmullRomSpline<Vector2>(controlPoints, true);
+        path3 = new Bezier<Vector2>(RossPoints);
 
         // setup ShapeRenderer
         game.shapeRenderer.setAutoShapeType(true);
@@ -114,7 +134,7 @@ public class MenuScreen implements Screen {
 
         game.shapeRenderer.setColor(1, 1, 1, 1);
         game.shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-
+/*
         //draw path1
         for(int i = 0; i < 100; ++i){
             float t = i /100f;
@@ -141,6 +161,120 @@ public class MenuScreen implements Screen {
             game.shapeRenderer.line(st.x, st.y, end.x, end.y);
 
         }
+
+        //Ross's curves
+        game.shapeRenderer.setColor(.5f, 1f, 1f, 1);
+        game.shapeRenderer.line(P0x, P0y, P1x, P1y);
+        game.shapeRenderer.line(P1x, P1y, P2x, P2y);
+        game.shapeRenderer.line(P0x, P0y, P2x, P2y);
+        game.shapeRenderer.setColor(1f, 1f, 1f, 1);
+        float segments = 100;
+        for(int i = 0; i < segments; ++i){
+            float t = i /segments;
+            Vector2 st = new Vector2();
+            Vector2 end = new Vector2();
+            path3.valueAt(st,t);
+            path3.valueAt(end, t-0.01f);
+
+            if(i == 50){
+                int breakpoint = 0;
+            }
+            game.shapeRenderer.line(st.x, st.y, end.x, end.y);
+
+        }
+*/
+        if(radiusFlux == 0){
+            radiusChange = 1;
+        }
+        if(radiusFlux > 10){
+            radiusChange = -1;
+        }
+        radiusFlux = radiusFlux + radiusChange;
+
+        int waveFrequency = 9;
+        float OriginX = 800;
+        float OriginY = 200;
+        float radius = Variables.HELIUM_RADIUS + radiusFlux;
+        float tempCureentDegrees = currentDegrees;
+        currentDegrees = currentDegrees + 2;
+        float nextDegrees;
+        float amplitude = radius/6f;
+        float segments = 10;
+
+        circlePath = new Bezier[waveFrequency * 2];
+
+        for(int i = 0; i < (waveFrequency * 2);i++){
+            nextDegrees = tempCureentDegrees + (360/(waveFrequency * 2));
+            Vector2[] currentPositions = new Vector2[3];
+            currentPositions[0] = new Vector2((float)Math.cos(Math.toRadians(tempCureentDegrees))*radius+OriginX,(float)Math.sin(Math.toRadians(tempCureentDegrees))*radius+OriginY);
+            currentPositions[2] = new Vector2((float)Math.cos(Math.toRadians(nextDegrees))*radius+OriginX,(float)Math.sin(Math.toRadians(nextDegrees))*radius+OriginY);
+
+            float midPointX = (currentPositions[0].x + currentPositions[2].x)/2;
+            float midPointY = (currentPositions[0].y + currentPositions[2].y)/2;
+            float midPointDistance = (float)Math.sqrt((midPointX - OriginX)*(midPointX - OriginX) + (midPointY - OriginY)*(midPointY - OriginY));
+            float bezierDistance = (radius - midPointDistance) + radius;
+            if(i%2 == 0){
+                bezierDistance = 2 * (radius - midPointDistance) + radius + amplitude;
+            }else{
+                bezierDistance = radius - (radius - midPointDistance)*2 - amplitude;
+            }
+
+            currentPositions[1] = new Vector2((float)Math.cos(Math.toRadians((tempCureentDegrees + nextDegrees)/2))*bezierDistance+OriginX,(float)Math.sin(Math.toRadians((tempCureentDegrees + nextDegrees)/2))*bezierDistance+OriginY);
+            circlePath[i] =  new Bezier<Vector2>(currentPositions);
+            tempCureentDegrees = nextDegrees;
+        }
+
+        game.shapeRenderer.setColor(0f, 1f, 0f, 1);
+
+        for(int i = 0; i < circlePath.length; i++){
+            for(int j = 0; j < segments; ++j){
+                float t = j /segments;
+                Vector2 st = new Vector2();
+                Vector2 end = new Vector2();
+                circlePath[i].valueAt(st,t);
+                circlePath[i].valueAt(end, t+1/(segments));
+                game.shapeRenderer.line(st.x, st.y, end.x, end.y);
+            }
+        }
+
+        tempCureentDegrees = -currentDegrees + (360/(waveFrequency * 2));
+
+        circlePath = new Bezier[waveFrequency * 2];
+
+        for(int i = 0; i < (waveFrequency * 2);i++){
+            nextDegrees = tempCureentDegrees + (360/(waveFrequency * 2));
+            Vector2[] currentPositions = new Vector2[3];
+            currentPositions[0] = new Vector2((float)Math.cos(Math.toRadians(tempCureentDegrees))*radius+OriginX,(float)Math.sin(Math.toRadians(tempCureentDegrees))*radius+OriginY);
+            currentPositions[2] = new Vector2((float)Math.cos(Math.toRadians(nextDegrees))*radius+OriginX,(float)Math.sin(Math.toRadians(nextDegrees))*radius+OriginY);
+
+            float midPointX = (currentPositions[0].x + currentPositions[2].x)/2;
+            float midPointY = (currentPositions[0].y + currentPositions[2].y)/2;
+            float midPointDistance = (float)Math.sqrt((midPointX - OriginX)*(midPointX - OriginX) + (midPointY - OriginY)*(midPointY - OriginY));
+            float bezierDistance = (radius - midPointDistance) + radius;
+            if(i%2 == 0){
+                bezierDistance = 2 * (radius - midPointDistance) + radius + amplitude;
+            }else{
+                bezierDistance = radius - (radius - midPointDistance)*2 - amplitude;
+            }
+
+            currentPositions[1] = new Vector2((float)Math.cos(Math.toRadians((tempCureentDegrees + nextDegrees)/2))*bezierDistance+OriginX,(float)Math.sin(Math.toRadians((tempCureentDegrees + nextDegrees)/2))*bezierDistance+OriginY);
+            circlePath[i] =  new Bezier<Vector2>(currentPositions);
+            tempCureentDegrees = nextDegrees;
+        }
+
+        game.shapeRenderer.setColor(0f, 1f, 0f, 1);
+
+        for(int i = 0; i < circlePath.length; i++){
+            for(int j = 0; j < segments; ++j){
+                float t = j /segments;
+                Vector2 st = new Vector2();
+                Vector2 end = new Vector2();
+                circlePath[i].valueAt(st,t);
+                circlePath[i].valueAt(end, t+1/(segments));
+                game.shapeRenderer.line(st.x, st.y, end.x, end.y);
+            }
+        }
+
 
         game.shapeRenderer.end();
     }
